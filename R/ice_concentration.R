3399e94e82304a435df89f0344fe3c553b86fc80
Philippe Massicotte <pmassicotte@hotmail.com>
2016-12-22 14:11:00 -0500

    work on preparing sea ice concentration data

--- R/ice_concentration.R
+++ R/ice_concentration.R
@@ -3,83 +3,110 @@
 
 rm(list = ls())
 
-baffin <- readOGR("data/shapefiles/baffin/", "baffin")
+extract_sic <- function(file, lon, lat, amundsen, proj) {
+  
+  date <- as.Date(stringr::str_match(file, "\\d{8}"), format = "%Y%m%d")
+  
+  # file <- "data/ice_concentration/Arc_20150301_res3.125_pyres.nc"
+  nc <- ncdf4::nc_open(file)
+  
+  sic <- ncdf4::ncvar_get(nc, "sea_ice_concentration") %>% 
+    as.vector()
+  
+  df <- data_frame(
+    lon = lon,
+    lat = lat,
+    sic = sic *  0.00999999977648258
+  ) %>% 
+    filter(lon >= -65 & lon <= -50) %>%
+    filter(lat >= 65 & lat <= 72) %>% 
+    mutate(date = date) %>% 
+    as.data.frame() %>% 
+    SpatialPointsDataFrame(cbind(.$lon, .$lat), data = ., proj4string = CRS(proj4string(area))) %>% 
+    spTransform(proj)
+  
+  ## Find the closest pixel for each station
+  snap <- apply(spDists(amundsen, df), 1, function(x) {
+    data_frame(distance_to_pixel = min(x),
+               which_min = which.min(x))
+  }) %>%
+    bind_rows()
 
-# rr2 <- crop(rr, baffin)
+  df <- data.frame(df) %>% 
+    dplyr::select(
+      lon_ice = lon, 
+      lat_ice = lat,
+      sic,
+      ice_date = date)
+    
+  res <- data.frame(amundsen) %>% 
+    bind_cols(df[snap$which_min, ]) %>% 
+    bind_cols(snap)
+  
+  return(res)
+}
 
-# ftp://ftp-projects.zmaw.de/seaice/AMSR2/
+## Have a look to spatial coverage
+area <- readOGR("data/shapefiles/", "area_of_interest")
+plot(rworldmap::getMap())
+plot(area, add = TRUE, col = "red")
 
-# dat <- ncdf4::nc_open("/home/pmassicotte/Downloads/Arc_20140429_res3.125_pyres.nc")
-# sic <- ncdf4::ncvar_get(dat, "sea_ice_concentration") %>% 
-#   as.vector()
-# 
-# latlong <- ncdf4::nc_open("/home/pmassicotte/Downloads/LongitudeLatitudeGrid_3.125km_Arctic.nc")
-# 
-# lat <- ncdf4::ncvar_get(latlong, "latitude") %>% 
-#   as.vector()
-# 
-# long <- ncdf4::ncvar_get(latlong, "longitude") %>% 
-#   as.vector()
+latlong <- ncdf4::nc_open("data/ice_concentration/LongitudeLatitudeGrid_3.125km_Arctic.nc")
 
-# longlat <- expand.grid(long, lat)
+lat <- ncdf4::ncvar_get(latlong, "latitude") %>%
+  as.vector()
 
-land_mask <- raster("/home/pmassicotte/Downloads/Arc_20140429_res3.125_pyres.nc", 
-                    varname = "land")
+lon <- ncdf4::ncvar_get(latlong, "longitude") %>%
+  as.vector()
 
-sic <- raster("/home/pmassicotte/Downloads/Arc_20140429_res3.125_pyres.nc", 
-              varname = "sea_ice_concentration") %>% 
-  mask(land_mask, maskvalue = 0) %>% 
-  rasterToPoints() %>% 
-  data.frame()
+proj <- "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 
-long <- raster("/home/pmassicotte/Downloads/LongitudeLatitudeGrid_3.125km_Arctic.nc", 
-                  varname = "longitude") %>% 
-  mask(land_mask, maskvalue = 0) %>% 
-  rasterToPoints() %>% 
-  data.frame()
+# *************************************************************************
+# Amundsen geographical positions
+# *************************************************************************
 
-lat <- raster("/home/pmassicotte/Downloads/LongitudeLatitudeGrid_3.125km_Arctic.nc", 
-               varname = "latitude") %>% 
-  mask(land_mask, maskvalue = 0) %>% 
-  rasterToPoints() %>% 
-  data.frame()
+amundsen <- readxl::read_excel("data/GE_Amundsen_Station_Coordinates.xlsx") %>% 
+  janitor::clean_names() %>% 
+  dplyr::select(cruise,
+                code_operation,
+                date_utc,
+                lat = lat_deg_n,
+                lon = long_deg_w,
+                station,
+                station_type) %>% 
+  mutate(date_utc = as.Date(date_utc, format = "%d-%b-%Y")) %>% 
+  mutate(lon = -lon) %>% 
+  filter(lat >= 65) %>% 
+  SpatialPointsDataFrame(cbind(.$lon, .$lat), data = ., proj4string = CRS(proj4string(area))) %>% 
+  spTransform(proj)
 
-df <- data_frame(
-  long = long$longitude,
-  lat = lat$latitude,
-  sic = sic$daily.averaged.total.ice.concentration
-) 
+files <- list.files("data/ice_concentration/", "2016\\d{4}\\S+pyres.nc$", full.names = TRUE)
 
+res <- lapply(files, extract_sic, lon = lon, lat = lat, amundsen = amundsen, proj = proj) %>% 
+  bind_rows() %>% 
+  as_tibble() %>% 
+  dplyr::select(-coords.x1, -coords.x2)
 
-# r <- rasterFromXYZ(df, digits = 0.25)
-# 
-# df %>% 
-#   ggplot(aes(x = long, y = lat, fill = sic)) +
-#   geom_raster()
-
-# crs(sic) <- proj4string(baffin)
-# 
-# extent(sic) <- extent(min(long$longitude), max(long$longitude), min(lat$latitude), max(lat$latitude))
-# 
-# proj <- "+proj=stere +lat_0=90 +lat_ts=70 +lon_0=-45 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-# 
-# sic2 <- sic %>% 
-#   crop(baffin) %>% 
-#   projectRaster(crs = proj)
-
-# df <- raster::crop(df, baffin)
+write_csv(res, "data/clean/ice_history.csv")
 
 
-# df %>% 
-#   ggplot(aes(x = long, y = lat, fill = sic)) +
-#   geom_raster() +
-#   scale_fill_viridis()
+## TEST
 
-sic %>%
-  ggplot(aes(x = x, y = y, fill = daily.averaged.total.ice.concentration)) +
-  geom_raster() +
-  scale_fill_viridis() +
-  geom_polygon(data = baffin,
-               aes(x = long, y = lat, group = group),
-               inherit.aes = FALSE) +
-  scale_x_continuous(labels = long$longitude)
+rm(list = ls())
+
+df <- read_csv("data/clean/ice_history.csv")
+
+unique(df$station)
+
+
+df %>% 
+  filter(station %in% c("Crossby", "G713", "DIC1")) %>% 
+  ggplot(aes(x = ice_date, y = sic, color = station)) +
+  geom_point() +
+  geom_line() +
+  theme(legend.justification = c(0, 0), legend.position = c(0.01, 0.01)) +
+  scale_x_date(date_labels = "%d-%b-%Y") +
+  xlab("Date") +
+  ylab("Sea ice concentration (%)")
+
+ggsave("graphs/sea_ice_concentration.pdf")
