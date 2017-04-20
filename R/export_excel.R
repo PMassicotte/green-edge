0cf4855996e87a8d2b5a663ad059b895905d6df1
Philippe Massicotte <pmassicotte@hotmail.com>
2017-04-20 14:32:25 -0400

    export_excel

---
+++ R/export_excel.R
@@ -0,0 +1,25 @@
+
+
+library(tidyverse)
+library(xlsx)
+
+export_excel <- function(file, overwrite = FALSE) {
+  
+  destfile <- paste0(file, ".xlsx")
+  
+  if (file.exists(destfile) & overwrite == FALSE) {
+    stop("File already exsists. Use overwrite = TRUE to force it.")
+  }
+  
+  df <- suppressWarnings(suppressMessages(read_csv(file, skip = 5, na = c("", "*****")))) %>% 
+    janitor::clean_names()
+  
+  
+  write.xlsx(as.data.frame(df), file = destfile, sheetName = basename(file), showNA = FALSE, row.names = FALSE)
+  
+  
+}
+
+
+# file <- "/home/pmassicotte/Downloads/Takuvik_14C_dpm.011"
+# export_excel(file, overwrite = TRUE)
