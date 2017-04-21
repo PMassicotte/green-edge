bdb8897c74f58868d35b29651b437d1ce69e5c7a
Philippe Massicotte <pmassicotte@hotmail.com>
2017-04-21 10:13:55 -0400

    update export

--- R/export_excel.R
+++ R/export_excel.R
@@ -17,16 +17,47 @@ export_excel <- function(file, overwrite = FALSE) {
   }
   
   df <- suppressWarnings(suppressMessages(read_csv(file, skip = 5, na = c("", "*****")))) %>% 
-    janitor::clean_names() %>% 
-    mutate(sample_id = NA) %>% 
-    mutate(measurement_type = NA) %>% 
-    mutate(light = NA) %>% 
-    mutate(dic = NA)
-    
+    janitor::clean_names() 
+  
+  write.xlsx(as.data.frame(df), file = destfile, sheetName = basename(file), showNA = FALSE, row.names = FALSE)
+  
+  df <- df %>% 
+    dplyr::select(-p, -pid, -s, dpm1) %>% 
+    mutate(station = NA) %>% 
+    mutate(cast = NA) %>% 
+    mutate(niskin = NA) %>% 
+    mutate(curve_id = NA) %>% 
+    mutate(measurement_type = NA) %>%
+    dplyr::select(station:measurement_type, dpm1) %>% 
+    mutate(light = NA)
+  
+  destfile <- paste0(file, "_input.xlsx")
+  
   write.xlsx(as.data.frame(df), file = destfile, sheetName = basename(file), showNA = FALSE, row.names = FALSE)
   
 }
 
+pvse_config_file <- function(dir, overwrite = FALSE) {
+  
+  if (dir.exists(dir) & overwrite == FALSE) {
+    stop("File already exsists. Use overwrite = TRUE to force it.")
+  }
+  
+  df <- data_frame(
+    station = NA,
+    cast = NA,
+    niskin = NA,
+    curve_id = NA,
+    volume_incubation_ml = NA,
+    volume_pipette_tc_ul = NA,
+    incubation_time_min = NA
+  )
+  
+  destfile <- paste0(dir, "/config_pvse_input.xlsx")
+  write.xlsx(as.data.frame(df), file = destfile, sheetName = "sheet1", showNA = FALSE, row.names = FALSE)
+  
+}
 
 # file <- "/home/pmassicotte/Downloads/Takuvik_14C_dpm.011"
 # export_excel(file, overwrite = TRUE)
+# pvse_config_file("/home/pmassicotte/Downloads/", overwrite = TRUE)
